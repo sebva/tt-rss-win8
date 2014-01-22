29218488436a55f5fb982e5cefd3fd5c02f7c041
Sébastien Vaucher <seb.vaucher@gmail.com>
2014-01-22 22:46:56 +0100

    Add missing file

---
+++ BoolToBoldConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Text;
+using Windows.UI.Xaml.Data;
+
+namespace TinyTinyRss
+{
+    public class BoolToBoldConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var read = (bool)value;
+            return read ? FontWeights.Normal : FontWeights.Bold;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
